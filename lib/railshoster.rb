eaa54938ba168098ac997b75173d52c73e05d323
Julian Fischer <fischer@enterprise-rails.de>
2011-11-03 17:22:33 +0800

    removed bundler for now. caused conflicts with Gemfiles in project folders to be deployed.

--- lib/railshoster.rb
+++ lib/railshoster.rb
@@ -1,8 +1,6 @@
 require File.join(File.dirname(__FILE__), 'railshoster/version')
 require File.join(File.dirname(__FILE__), 'railshoster/init_command')
 
-require 'bundler/setup'
-
 module Railshoster
   # Your code goes here...
 end
