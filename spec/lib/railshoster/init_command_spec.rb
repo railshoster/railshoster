d31b820b16bf91e4652e5b90aeddfd7c9e7d1735
Julian Fischer <fischer@enterprise-rails.de>
2011-11-04 23:11:54 +0800

    added rspec scaffold

---
+++ spec/lib/railshoster/init_command_spec.rb
@@ -0,0 +1,7 @@
+require File.join(File.dirname(__FILE__), "..", "..", "spec_helper.rb")
+
+describe RailsHoster::InitCommand do
+  it "should test nothing" do
+    true.should be(true)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'railshoster')
+
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
